a318b87a455e46fd6d2b86d239cbb9d844afac0e
Dung <vuquocdung204@gmail.com>
2026-01-03 00:54:04 +0700

    cp logic

--- Assets/Dung_Dev/SpotController.cs
+++ Assets/Dung_Dev/SpotController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class SpotController : MonoBehaviour
@@ -19,13 +20,108 @@ public class SpotController : MonoBehaviour
             Debug.Log("Spot is full");
             return;
         }
+        int indexEmpty = FindInsertSameType(fishSelected.id);
         
-        
-        int indexEmpty = GetFirstEmptySpot();
+        MoveAllToRight(indexEmpty);
         
         spots[indexEmpty].SetFish(fishSelected);
         fishSelected.MoveToSpot(spots[indexEmpty]);
+
+        DOVirtual.DelayedCall(fishSelected.moveDuration + 0.01f, delegate
+        {
+            HandleMatch3();
+        });
     }
+
+    private void HandleMatch3()
+    {
+        List<Fish> listToDestroy =  new List<Fish>();
+
+        for (int i = 0; i < spots.Count - 2; i++)
+        {
+            if (!spots[i].IsEmpty() && !spots[i + 1].IsEmpty() && !spots[i + 2].IsEmpty())
+            {
+                var fish0 = spots[i].fish;
+                var fish1 = spots[i + 1].fish;
+                var fish2 = spots[i + 2].fish;
+
+                if (fish0.id == fish1.id && fish2.id == fish1.id)
+                {
+                    listToDestroy.Add(fish0);
+                    listToDestroy.Add(fish1);
+                    listToDestroy.Add(fish2);
+                    
+                    spots[i].SetFish(null);
+                    spots[i + 1].SetFish(null);
+                    spots[i + 2].SetFish(null);
+                }
+            }
+        }
+        if (listToDestroy.Count >= 3)
+        {
+            for(int i = listToDestroy.Count - 1; i >= 0; i--)
+                Destroy(listToDestroy[i].gameObject);
+            
+            MoveAllToLeft();
+            Debug.Log("Match 3");
+        }
+    }
+
+    private void MoveAllToLeft()
+    {
+        List<Fish> activeFishInSpot = new List<Fish>();
+        
+        for (int i = 0; i < spots.Count; i++)
+        {
+            if (!spots[i].IsEmpty())
+            {
+                activeFishInSpot.Add(spots[i].fish);
+                spots[i].SetFish(null);
+            }
+        }
+
+        for (int i = 0; i < activeFishInSpot.Count; i++)
+        {
+            spots[i].SetFish(activeFishInSpot[i]);
+            activeFishInSpot[i].MoveToSpot(spots[i]);
+        }
+    }
+    
+    private void MoveAllToRight(int insertIndex)
+    {
+        int firstEmptySpot = GetFirstEmptySpot();
+        if(firstEmptySpot == insertIndex)  return;
+
+        for (int i = firstEmptySpot; i > insertIndex; i--)
+        {
+            Spot currentSpot = spots[i];
+            Spot prevSpot = spots[i - 1];
+            
+            currentSpot.SetFish(prevSpot.fish);
+            prevSpot.SetFish(null);
+            
+            currentSpot.fish.MoveToSpot(currentSpot);
+        }
+    }
+
+    private int FindInsertSameType(int fishId)
+    {
+        int lastIndexSameType = -1;
+        
+        for (int i = 0; i < spots.Count; i++)
+        {
+            if (!spots[i].IsEmpty() && spots[i].fish.id == fishId)
+            {
+                lastIndexSameType = i;
+            }
+        }
+
+        if (lastIndexSameType == -1)
+            return GetFirstEmptySpot();
+        
+        return lastIndexSameType + 1;
+    }
+    
 
     private int GetFirstEmptySpot()
     {
